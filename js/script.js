77794485181461f41943bbdf9c5b587e6db058ef
seeseesky <ccskytpk@gmail.com>
2019-01-22 01:31:14 +0800

    first commit

---
+++ js/script.js
@@ -0,0 +1,197 @@
+/*
+
+Style   : MobApp Script JS
+Version : 1.0
+Author  : Surjith S M
+URI     : https://surjithctly.in/
+
+Copyright © All rights Reserved
+
+*/
+
+$(function() {
+    "use strict";
+
+    /*-----------------------------------
+     * FIXED  MENU - HEADER
+     *-----------------------------------*/
+    function menuscroll() {
+        var $navmenu = $('.nav-menu');
+        if ($(window).scrollTop() > 50) {
+            $navmenu.addClass('is-scrolling');
+        } else {
+            $navmenu.removeClass("is-scrolling");
+        }
+    }
+    menuscroll();
+    $(window).on('scroll', function() {
+        menuscroll();
+    });
+    /*-----------------------------------
+     * NAVBAR CLOSE ON CLICK
+     *-----------------------------------*/
+
+    $('.navbar-nav > li:not(.dropdown) > a').on('click', function() {
+        $('.navbar-collapse').collapse('hide');
+    });
+    /*
+     * NAVBAR TOGGLE BG
+     *-----------------*/
+    var siteNav = $('#navbar');
+    siteNav.on('show.bs.collapse', function(e) {
+        $(this).parents('.nav-menu').addClass('menu-is-open');
+    })
+    siteNav.on('hide.bs.collapse', function(e) {
+        $(this).parents('.nav-menu').removeClass('menu-is-open');
+    })
+
+    /*-----------------------------------
+     * ONE PAGE SCROLLING
+     *-----------------------------------*/
+    // Select all links with hashes
+    $('a[href*="#"]').not('[href="#"]').not('[href="#0"]').not('[data-toggle="tab"]').on('click', function(event) {
+        // On-page links
+        if (location.pathname.replace(/^\//, '') == this.pathname.replace(/^\//, '') && location.hostname == this.hostname) {
+            // Figure out element to scroll to
+            var target = $(this.hash);
+            target = target.length ? target : $('[name=' + this.hash.slice(1) + ']');
+            // Does a scroll target exist?
+            if (target.length) {
+                // Only prevent default if animation is actually gonna happen
+                event.preventDefault();
+                $('html, body').animate({
+                    scrollTop: target.offset().top
+                }, 1000, function() {
+                    // Callback after animation
+                    // Must change focus!
+                    var $target = $(target);
+                    $target.focus();
+                    if ($target.is(":focus")) { // Checking if the target was focused
+                        return false;
+                    } else {
+                        $target.attr('tabindex', '-1'); // Adding tabindex for elements not focusable
+                        $target.focus(); // Set focus again
+                    };
+                });
+            }
+        }
+    });
+    /*-----------------------------------
+     * OWL CAROUSEL
+     *-----------------------------------*/
+    var $testimonialsDiv = $('.testimonials');
+    if ($testimonialsDiv.length && $.fn.owlCarousel) {
+        $testimonialsDiv.owlCarousel({
+            items: 1,
+            nav: true,
+            dots: false,
+            navText: ['<span class="ti-arrow-left"></span>', '<span class="ti-arrow-right"></span>']
+        });
+    }
+
+    var $galleryDiv = $('.img-gallery');
+    if ($galleryDiv.length && $.fn.owlCarousel) {
+        $galleryDiv.owlCarousel({
+            nav: false,
+            center: true,
+            loop: true,
+            autoplay: true,
+            dots: true,
+            navText: ['<span class="ti-arrow-left"></span>', '<span class="ti-arrow-right"></span>'],
+            responsive: {
+                0: {
+                    items: 1
+                },
+                768: {
+                    items: 3
+                }
+            }
+        });
+    }
+
+}); /* End Fn */
+
+function main() {
+
+(function () {
+   'use strict';
+
+  	$('a.page-scroll').click(function() {
+        if (location.pathname.replace(/^\//,'') == this.pathname.replace(/^\//,'') && location.hostname == this.hostname) {
+          var target = $(this.hash);
+          target = target.length ? target : $('[name=' + this.hash.slice(1) +']');
+          if (target.length) {
+            $('html,body').animate({
+              scrollTop: target.offset().top - 40
+            }, 900);
+            return false;
+          }
+        }
+      });
+
+
+    // Show Menu on Book
+    $(window).bind('scroll', function() {
+        var navHeight = $(window).height() - 500;
+        if ($(window).scrollTop() > navHeight) {
+            $('.navbar-default').addClass('on');
+        } else {
+            $('.navbar-default').removeClass('on');
+        }
+    });
+
+    $('body').scrollspy({
+        target: '.navbar-default',
+        offset: 80
+    });
+
+	// Hide nav on click
+  $(".navbar-nav li a").click(function (event) {
+    // check if window is small enough so dropdown is created
+    var toggle = $(".navbar-toggle").is(":visible");
+    if (toggle) {
+      $(".navbar-collapse").collapse('hide');
+    }
+  });
+
+  $('#askus').click(function(evt){
+    $('#askus').prop( "disabled", true );
+    $('#askus').html('正在送出訊息...');
+    evt.preventDefault();
+    var email = $('#email').val();
+    var name = $('#name').val();
+    var message = $('#message').val();
+    var templateParams = {
+      from_name: name,
+      from_email: email,
+      message_html: message
+    };
+
+    if(!email || !message) {
+      alert('請填寫電郵及訊息');
+      $('#askus').prop( "disabled", false );
+      $('#askus').html('送出訊息');
+      return;
+    }
+
+    emailjs.send('zoho', 'template_Ho1CodxE', templateParams)
+        .then(function(response) {
+           alert('感謝，訊息已送出，我們會盡快回覆你的查詢。');
+           $('#email').val('');
+           $('#name').val('');
+           $('#message').val('');
+           $('#askus').prop( "disabled", false );
+           $('#askus').html('送出訊息');
+        }, function(error) {
+           alert('無法送出訊息，請稍後再試或透過其他方式聯絡我們');
+           $('#askus').prop( "disabled", false );
+           $('#askus').html('送出訊息');
+        });
+  });
+
+
+}());
+
+
+}
+main();
